f28e0b2b6c66abb0be1682808691ec946cac9fc5
Bruno Morais <brunosmmm@gmail.com>
2019-06-05 11:04:12 -0400

    production secret key

--- chainball/settings.py
+++ chainball/settings.py
@@ -19,11 +19,15 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "blabla"
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
+
+if DEBUG is False:
+    # SECURITY WARNING: keep the secret key used in production secret!
+    with open("/etc/secret_key.txt") as f:
+        SECRET_KEY = f.read().strip()
+else:
+    SECRET_KEY = "blabla"
 
 ALLOWED_HOSTS = []
 
