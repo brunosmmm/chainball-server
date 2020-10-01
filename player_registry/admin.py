fb25246a5974867c16d8cf7419adfdff5b7c3870
Bruno Morais <brunosmmm@gmail.com>
2020-10-01 10:18:21 -0400

    Alphabetically sort players

--- player_registry/admin.py
+++ player_registry/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from .models import Player
 
-# Register your models here.
-admin.site.register(Player)
+
+class PlayerAdmin(admin.ModelAdmin):
+    ordering = ["name"]
+
+
+admin.site.register(Player, PlayerAdmin)
